032eb0a414ddeeef1f243c109b41470db7db17f7
lesguillemets <kgkacko+github@gmail.com>
2024-04-16 15:52:42 +0900

    blocking

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
+static BASE_URL: &str = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/";
+
 pub fn f() {
     println!("Hello, world!");
 }
 
+pub struct Search {
+    pub database: String,
+    pub query: String,
+}
+
+impl ToURL for Search {
+    fn to_url(&self) -> String {
+        format!(
+            "{BASE_URL}/esearch.fcgi?db={}&term={}",
+            self.database, self.query
+        )
+    }
+}
+
+pub trait ToURL {
+    fn to_url(&self) -> String;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-use talk_ncbi;
+use reqwest;
+use talk_ncbi::ToURL;
 fn main() {
     talk_ncbi::f();
+    let db = String::from("pubmed");
+    let q = String::from("science[journal]+AND+breast+cancer+AND+2008[pdat]");
+    let s = talk_ncbi::Search {
+        database: db,
+        query: q,
+    };
+    println!("{}", g(&s).unwrap());
+}
+
+fn g(s: &talk_ncbi::Search) -> Option<String> {
+    println!("looking at -> {}", s.to_url());
+    let body = reqwest::blocking::get(s.to_url()).ok()?.text().ok()?;
+    return Some(body);
 }
